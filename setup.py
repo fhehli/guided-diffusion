4a46d026b79a4000b86302d59ac9f1f3e68bffa0
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-09 17:33:35 +0200

    add find_package to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="guided-diffusion",
+    packages=find_packages(),
     py_modules=["guided_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
